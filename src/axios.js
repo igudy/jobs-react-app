b030d609e7e60f4b7f0140719231dba2308bd31c
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-24 16:56:22 +0100

    Endpoint working properly

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 // axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
-axios.defaults.baseURL = "http://localhost:3000/api/v1"
+axios.defaults.baseURL = "https://jobs-api-25or.onrender.com/api/v1"
 
 axios.interceptors.request.use(function (req) {
   const user = localStorage.getItem("user")
